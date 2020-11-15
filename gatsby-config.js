ba00cdd9edc3f144535ea89f13bafc71258a3c1e
Alice Oh <helloaliceoh@gmail.com>
2020-11-15 14:43:27 +0800

    do not hardcode siteUrl field

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === `prod` && config.siteUrl === `http://localhost:8000
 module.exports = {
     pathPrefix: `/blog`,
     siteMetadata: {
-        siteUrl: `https://kind-mcclintock-b2d902.herokuapp.com/blog`,
+        siteUrl: process.env.SITEURL || config.siteUrl,
     },
     plugins: [
         /**
